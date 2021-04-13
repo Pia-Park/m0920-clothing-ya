83d91d18e1631e36c70a7ce9751afd2e9debc54a
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-04-13 11:56:52 -0700

    added sign in using firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Switch, Route} from 'react-router-dom'
 import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 // const HatsPage = () => {
 //   <div>
@@ -25,6 +26,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
         <Route component={Page404} />
       </Switch>
     </div>
