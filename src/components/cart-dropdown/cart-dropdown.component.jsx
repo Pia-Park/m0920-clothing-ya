4f63cfe1476f098efb9f98661978ccd81a799506
仁愛 朴 <inae1park@gmail.com>
2021-04-15 09:35:44 -0700

    added cart

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './cart-dropdown.styles.scss'
+
+import CartItem from '../cart-item/cart-item.component'
+import CustomButton from '../custom-button/custom-button.component'
+
+
+const CartDropdown = ({ cartItems }) => {
+
+    return (
+        <div className="cart-dropdown">
+            <div className="cart-itmes">               
+                {
+                    cartItems.length ? 
+                    cartItems.map((cartItem) => (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    )) : (
+                        <span className="empty-message">Your cart is empty</span>
+                    )
+                }
+            </div>
+            <CustomButton>
+                Go to Checkout
+            </CustomButton>
+
+        </div>
+    )
+}
+
+export default CartDropdown
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom'
 
 import Logo from '../../assets/ClothingYa.png'
 import {auth} from '../../firebase/firebase.util'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 import './header.styles.scss'
 
@@ -15,14 +17,18 @@ const Header = ({currentUser}) => (
             <Link className="option" to="/shop">
                 Shop
             </Link>
+
             {
                 currentUser ? (<div className="option" onClick={()=> auth.signOut()}>Sign out</div>)
                 :(<Link className="option" to="/signin">
                 Sign In </Link>)
             }
-            {/* <Link className="option" to="/signin">
-                Sign In 
-            </Link> */}
+        
+
+            {/* <CartDropdown cartItems={[]}/> */}
+            <CartIcon />
+            {/* {hidden ? null : <CartDropdown />} */}
+
 
         </div>
     </div>
